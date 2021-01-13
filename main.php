8ea8d851fc300c1f91c1081cdf5fffbcc816ca9f
Mike Stratton <mike@mikestratton.net>
2021-01-13 22:48:32 +0000

    txt

--- main.php
+++ main.php
@@ -1,2 +1,2 @@
 <?php
-echo "Hello, Aggressive Cow! \nLet's implement the binary search algorithm so you cannot attack another cow! An aggressive cow is a lonely cow.";
+echo "Hello, Aggressive Cow! \nLet's implement the binary search algorithm so you cannot attack another cow!";
